54f99adbd9802cc1ddd9928e8eb6917c9b998f4a
Conrad Rider <conrad.rider@flarebright.com>
2019-05-23 12:56:59 +0700

    Corrected indentation

--- bamboo.js
+++ bamboo.js
@@ -1,36 +1,35 @@
 'use strict';
 
 var inherit = require('inherit'),
-    _ = require('lodash'),
-    chalk = require('chalk'),
+	_ = require('lodash'),
+	chalk = require('chalk'),
 	fs = require('fs'),
+	RunnerEvents = require('../gemini/lib/constants/events'),
 
-    RunnerEvents = require('../gemini/lib/constants/events'),
-
-    ICON_SUCCESS = chalk.green('\u2713'),
-    ICON_FAIL = chalk.red('\u2718'),
-    ICON_WARN = chalk.bold.yellow('!');
+ICON_SUCCESS = chalk.green('\u2713'),
+ICON_FAIL = chalk.red('\u2718'),
+ICON_WARN = chalk.bold.yellow('!');
 
 var Runner = inherit({
-    attachRunner: function(runner) {
-        runner.on(RunnerEvents.TEST_RESULT, this._onTestResult.bind(this));
-        runner.on(RunnerEvents.BEGIN, this._onBegin.bind(this));
-        runner.on(RunnerEvents.END_TEST, this._onEndTest.bind(this));
-        runner.on(RunnerEvents.WARNING, this._onWarning.bind(this));
-        runner.on(RunnerEvents.END, this._onEnd.bind(this));
-        runner.on(RunnerEvents.INFO, this._onInfo.bind(this));
-        runner.on(RunnerEvents.SKIP_STATE, this._onSkipState.bind(this));
-    },
+	attachRunner: function(runner){
+			runner.on(RunnerEvents.TEST_RESULT, this._onTestResult.bind(this));
+			runner.on(RunnerEvents.BEGIN, this._onBegin.bind(this));
+			runner.on(RunnerEvents.END_TEST, this._onEndTest.bind(this));
+			runner.on(RunnerEvents.WARNING, this._onWarning.bind(this));
+			runner.on(RunnerEvents.END, this._onEnd.bind(this));
+			runner.on(RunnerEvents.INFO, this._onInfo.bind(this));
+			runner.on(RunnerEvents.SKIP_STATE, this._onSkipState.bind(this));
+	},
 
-    _compile: function(tmpl, data) {
-        return _.template(tmpl, {
-            imports: {
-                chalk: chalk
-            }
-        })(data);
-    },
+	_compile: function(tmpl, data){
+		return _.template(tmpl, {
+				imports: {
+						chalk: chalk
+				}
+		})(data);
+	},
 
-    _onBegin: function() {
+	_onBegin: function(){
 		this.beginTestTime = this.startTime = new Date();
 		this.suiteSet = {};
 		this.results = {
@@ -39,53 +38,52 @@ var Runner = inherit({
 			failures:[],
 			skipped:[]
 		};
-    },
-
-    _onEndTest: function(result) {
-        var handler = result.equal? this._onCapture : this._onError;
-        handler.call(this, result);
-    },
-
-    _onTestResult(result) {
-			const handler = result.equal ? this._onPassed : this._onError;
-			handler.call(this, result);
 	},
 
-    _onPassed: function(result) {
+	_onEndTest: function(result){
+		var handler = result.equal? this._onCapture : this._onError;
+		handler.call(this, result);
+	},
+
+	_onTestResult(result){
+		const handler = result.equal ? this._onPassed : this._onError;
+		handler.call(this, result);
+	},
+
+	_onPassed: function(result){
 		var pass = this._createResult(result);
 		this.results.passes.push(pass);
 	},
 
-    _onError: function(result) {
+	_onError: function(result){
 		var error = this._createResult(result);
 		error.error = result.message;
 		this.results.failures.push(error);
-    },
+	},
 
-    _onWarning: function(result) {
+	_onWarning: function(result){
 		var warn = this._createResult(result);
 		warn.warning = result.message;
 		this.results.skipped.push(warn);
-    },
+	},
 
-    _onSkipState: function(result) {
+	_onSkipState: function(result){
 		var warn = this._createResult(result);
 		warn.warning = result.message;
 		this.results.skipped.push(warn);
-    },
+	},
 
-    _onInfo: function(result) {
-        console.log(result.message);
-    },
+	_onInfo: function(result){
+		console.log(result.message);
+	},
 
-    _onEnd: function() {
-        var total = this.results.failures.length + this.results.passes.length + this.results.skipped.length;
-        console.log('Total: %s Passed: %s Failed: %s Skipped: %s',
-            chalk.underline(total),
-            chalk.green(this.results.passes.length),
-            chalk.red(this.results.failures.length),
-            chalk.cyan(this.results.skipped.length)
-        );
+	_onEnd: function(){
+		var total = this.results.failures.length + this.results.passes.length + this.results.skipped.length;
+		console.log('Total: %s Passed: %s Failed: %s Skipped: %s',
+				chalk.underline(total),
+				chalk.green(this.results.passes.length),
+				chalk.red(this.results.failures.length),
+				chalk.cyan(this.results.skipped.length));
 		var endTime = new Date();
 		var suites = 0;
 		for(var k in this.suiteSet){
@@ -103,7 +101,7 @@ var Runner = inherit({
 		};
 		// Write test results as parsable JSON file
 		fs.writeFile('gemini-bamboo.json', JSON.stringify(this.results, null, 2));
-    },
+	},
 
 	_createResult: function(result){
 		var obj = {};
@@ -126,5 +124,5 @@ var Runner = inherit({
 
 module.exports = function(gemini, options){
 	var runner = new Runner();
-    gemini.on('startRunner', runner.attachRunner.bind(runner));
+	gemini.on('startRunner', runner.attachRunner.bind(runner));
 };
